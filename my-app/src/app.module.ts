172a32b1bf694c1ffc306c8fad859a60099e0783
Kaihanafusa <85861384+Kaihanafusa@users.noreply.github.com>
2024-01-20 16:11:51 +0900

    add my-app

---
+++ my-app/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from 'src/prisma.service';
+import { PostsResolver } from './posts/posts.resolver';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+    controllers: [AppController],
+    providers: [AppService, PrismaService, PostsResolver],
+})
+export class AppModule {}
